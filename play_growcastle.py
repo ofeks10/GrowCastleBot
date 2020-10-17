ed018c432df0ba3824999972caea6cb486180ac7
ofeks10 <ofeks10@gmail.com>
2020-10-17 20:46:58 +0300

    some cleaning up of the code

--- play_growcastle.py
+++ play_growcastle.py
@@ -118,34 +118,16 @@ def main():
             'end_y': end_y
         })
         search_thread.start()
+        time.sleep(.1)
+        is_in_menu = False
 
         # Search for Tounge Chest and click Alice
         while not is_in_menu:
             im = region_grabber((start_x, start_y, end_x, end_y))
+            # Search for chest and witches and click them if we find them
             click_image_relative_to_window('./references/tongue_chest.png', start_x, start_y, end_x, end_y, im=im)
-            
-            # if pos[0] != -1:
-            #     position_for_window = [0, 0]
-            #     position_for_window[0] = pos[0] + start_x
-            #     position_for_window[1] = pos[1] + start_y
-            #     print('cliking chest in', position_for_window)
-            #     click_image('./references/tongue_chest.png', position_for_window, 'left', .2, offset=7)
-
             click_image_relative_to_window('./references/Alice.png', start_x, start_y, end_x, end_y, offset=2, precision=0.92, im=im)
-            time.sleep(random.randint(0, 3) / 10)
             click_image_relative_to_window('./references/Lisa.png', start_x, start_y, end_x, end_y, offset=2, precision=0.92, im=im)
-            # if alice_pos[0] != -1 or lisa_pos[0] != -1:
-            #     alice_position_for_window = [0, 0]
-            #     alice_position_for_window[0] = alice_pos[0] + start_x
-            #     alice_position_for_window[1] = alice_pos[1] + start_y
-            #     lisa_position_for_window = [0, 0]
-            #     lisa_position_for_window[0] = lisa_pos[0] + start_x
-            #     lisa_position_for_window[1] = lisa_pos[1] + start_y
-                # print('clicking witches in: ', alice_position_for_window, lisa_position_for_window)
-                # if alice_pos[0]  != -1:
-                #     click_image('./references/Alice.png', alice_position_for_window, 'left', .1, offset=2)
-                # if lisa_pos[0] != -1:
-                #     click_image('./references/Lisa.png', lisa_position_for_window, 'left', .1, offset=2)
             time.sleep(.1)
         search_thread.join()
         print('finished one level, replaying')
